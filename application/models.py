fab2b26b9ff97078f9f763d6badb8aa9b20c56d6
Ernan Hughes <155574808+ernanhughes@users.noreply.github.com>
2024-08-08 23:03:32 +0100

    initial commit

---
+++ application/models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Setting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(50), unique=True, nullable=False)
+    value = db.Column(db.String(200), nullable=False)
---
+++ application/settings.py
@@ -0,0 +1,14 @@
+from application.models import db, Setting
+
+def save_setting(key, value):
+    setting = Setting.query.filter_by(key=key).first()
+    if setting:
+        setting.value = value
+    else:
+        setting = Setting(key=key, value=value)
+        db.session.add(setting)
+    db.session.commit()
+
+def load_setting(key):
+    setting = Setting.query.filter_by(key=key).first()
+    return setting.value if setting else None
